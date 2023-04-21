4f7adc71ddae096edaae0be0f8d35c31e0c00c1f
lexx24061971 <130874209+lexx24061971@users.noreply.github.com>
2023-04-22 02:47:43 +0400

    В Лэйбл введена переменная (а) для счетчика, написаны комментарии

--- superCounterTwo/ViewController.swift
+++ superCounterTwo/ViewController.swift
@@ -9,15 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var counter: UILabel!
-    @IBOutlet weak var history: UITextField!
-    @IBOutlet weak var reset: UIButton!
-    @IBOutlet weak var minus: UIButton!
-    @IBOutlet weak var plus: UIButton!
-    var a = 0
+    @IBOutlet weak var counter: UILabel! // поле счетчика
+    @IBOutlet weak var history: UITextField! // текстовое поле истории изменений
+    @IBOutlet weak var reset: UIButton! // кнопка обнуления
+    @IBOutlet weak var minus: UIButton! //кнопка убавить счетчик
+    @IBOutlet weak var plus: UIButton! // кнопка прибавить счетчик
+    var a = 0 // переменная для счетчика
     override func viewDidLoad() {
         super.viewDidLoad()
-        counter.text = "Значение счётчика: 0"
+        counter.text = "Значение счётчика: \(a)"
         // Do any additional setup after loading the view.
     }
 
