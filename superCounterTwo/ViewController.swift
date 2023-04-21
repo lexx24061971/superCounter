3be782465d6d11938643d63e7d7541a216826383
lexx24061971 <130874209+lexx24061971@users.noreply.github.com>
2023-04-22 02:43:56 +0400

    Созданы лэйбл, текстовое поле, кнопки, у лэйбла установлена связь с кодом

--- superCounterTwo/ViewController.swift
+++ superCounterTwo/ViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var history: UITextField!
+    @IBOutlet weak var reset: UIButton!
+    @IBOutlet weak var minus: UIButton!
+    @IBOutlet weak var plus: UIButton!
+    var a = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        counter.text = "Значение счётчика: 0"
         // Do any additional setup after loading the view.
     }
 
