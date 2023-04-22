4711df490bfeb021dc180202e2743483f16236d8
lexx24061971 <130874209+lexx24061971@users.noreply.github.com>
2023-04-22 17:21:19 +0400

    Исправлена ошибка расположения строки для изменения значения счетчика в функции reductionCounter()

--- superCounterTwo/ViewController.swift
+++ superCounterTwo/ViewController.swift
@@ -21,37 +21,39 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         counter.text = "Значение счётчика: \(counterValue)"
     }
-    
-    
-    @IBAction private func reset(_ sender: Any) {
-        resetCounter()
-    }
-    private func resetCounter() {
-        counterValue = 0
-        history.text = "\(dateAndTime) значение сброшено"
-    }
-    
-    
-    @IBAction private func plus(_ sender: Any) {
-        incrementCounter()
-    }
-    
-    private func incrementCounter() {
-        counterValue += 1
-        history.text = "\(dateAndTime) значение изменено на +1"
-    }
-    
-    @IBAction private func minus(_ sender: Any) {
-        reductionCounter()
+        
+        @IBAction private func reset(_ sender: Any) {
+            resetCounter()
         }
-    private func reductionCounter() {
-        counterValue -= 1
+        private func resetCounter() {
+            counterValue = 0
+            counter.text = "Значение счётчика: \(counterValue)"
+            history.text = "\(dateAndTime) значение сброшено"
+        }
+        
+        
+        @IBAction private func plus(_ sender: Any) {
+            incrementCounter()
+        }
+        
+        private func incrementCounter() {
+            counterValue += 1
+            counter.text = "Значение счётчика: \(counterValue)"
+            history.text = "\(dateAndTime) значение изменено на +1"
+        }
+        
+        @IBAction private func minus(_ sender: Any) {
+            reductionCounter()
+        }
+        private func reductionCounter() {
+            counterValue -= 1
+            counter.text = "Значение счётчика: \(counterValue)"
             history.text = "\(dateAndTime) значение изменено на -1"
             if counterValue < 0 {
                 counterValue = 0
                 history.text = "\(dateAndTime) попытка уменьшить значение счётчика ниже 0"
+            }
         }
-    }
     
 }
 
