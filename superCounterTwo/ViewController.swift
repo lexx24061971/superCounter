c498a41ddbcd57e35f0df0cf9db482e733dceb59
lexx24061971 <130874209+lexx24061971@users.noreply.github.com>
2023-04-22 16:38:47 +0400

    Функции сделаны приватными, введены дополнительные функции для действий, изменены имена переменных

--- superCounterTwo/ViewController.swift
+++ superCounterTwo/ViewController.swift
@@ -14,40 +14,44 @@ class ViewController: UIViewController {
     @IBOutlet weak private var reset: UIButton! // reset button
     @IBOutlet weak private var minus: UIButton! //button to reduce the counter
     @IBOutlet weak private var plus: UIButton! // add counter button
-    var a = 0 // variable for the counter
+    var counterValue = 0 // variable for the counter
+    var dateAndTime = NSDate() //variable for displaying the current date and time
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        counter.text = "Значение счётчика: \(a)"
+        counter.text = "Значение счётчика: \(counterValue)"
     }
-    var b = NSDate() //variable for displaying the current date and time
     
     
     @IBAction private func reset(_ sender: Any) {
-        a = 0
-        history.text = "\(b) значение сброшено"
-        viewDidLoad()
+        resetCounter()
     }
-    //actions when pressing the reset button
+    private func resetCounter() {
+        counterValue = 0
+        history.text = "\(dateAndTime) значение сброшено"
+    }
     
     
     @IBAction private func plus(_ sender: Any) {
-        a += 1
-        history.text = "\(b) значение изменено на +1"
-        viewDidLoad()
+        incrementCounter()
     }
     
-    // actions when clicking on the plus button
+    private func incrementCounter() {
+        counterValue += 1
+        history.text = "\(dateAndTime) значение изменено на +1"
+    }
     
     @IBAction private func minus(_ sender: Any) {
-        a -= 1
-        history.text = "\(b) значение изменено на -1"
-        if a < 0 {
-            a = 0
-            history.text = "\(b) попытка уменьшить значение счётчика ниже 0"
+        reductionCounter()
         }
-        viewDidLoad()
+    private func reductionCounter() {
+        counterValue -= 1
+            history.text = "\(dateAndTime) значение изменено на -1"
+            if counterValue < 0 {
+                counterValue = 0
+                history.text = "\(dateAndTime) попытка уменьшить значение счётчика ниже 0"
+        }
     }
-    // actions when clicking on the minus button
     
 }
 
